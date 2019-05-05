871070e7a8968007629769f2f895622195b43c6c
Stephanie Angulo <angulostephanie21@gmail.com>
2019-05-04 17:42:28 -0700

    more clean up

--- src/main/java/Server/UserController.java
+++ src/main/java/Server/UserController.java
@@ -1,19 +1,11 @@
 package Server;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.*;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.sql.*;
-import java.util.Arrays;
-import java.util.Collections;
 
 import org.json.JSONObject;
 
